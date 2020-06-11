8b5ba6ef8bb2e755930432efba4415c4881b7b80
viktor <victor.kutsiy@ya.ru>
2020-06-11 15:38:01 +0600

    mirror pyramid

--- pyramid.cpp
+++ pyramid.cpp
@@ -9,11 +9,11 @@ void main()
 	cout << "Enter the height of the pyramid : " << endl;
 	cin >> a;
 
-	for (int i = 0; i < a; i++)
+	for (int i = 1; i <= a * 2 - 1; i++)
 	{
-		for (int j = 0; j < a * 2 - 1; j++)
+		for (int j = 1; j <= a * 2 - 1; j++)
 		{
-			if (j >= a - i - 1 && j <= a + i - 1)
+			if (j >= a - i + 1 && j <= a + i - 1 && j >= i - a + 1 && j <= (a * 2) - i + (a - 1))
 			{
 				cout << "*";
 			}
